f0c963b8ccd6463aa67974fc40ceb5d542ba0e2e
Robert Calistri <robertcalistri@Roberts-Air-2.lan>
2023-07-21 13:08:25 -0400

    Removed ratings/review fields from Inventory, created Reviews model

--- app/models/inventory.py
+++ app/models/inventory.py
@@ -8,10 +8,7 @@ class Inventory(db.Model):
     product_name = CharField(max_length=255)
     product_cost = DecimalField(decimal_places=2)
     product_stock = IntegerField()
-    product_rating = DecimalField(decimal_places=2)
-    product_review = CharField(max_length=255)
     
-
     class Meta:
         database = db
         table_name = 'Inventory'
