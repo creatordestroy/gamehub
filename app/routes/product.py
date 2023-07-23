101f093586aeb6add6c5548878d19c0d425da94c
jime3160 <jime3160@gmail.com>
2023-07-23 16:17:31 -0400

    Created Product List and Product Detail with ability to discontinue product. Closes #29

---
+++ app/routes/product.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from app.services.product_services import ProductService
+
+bp = Blueprint('product', __name__)
+
+@bp.route('/list/')
+def product_list():
+    product_service = ProductService()
+    products = product_service.get_product_list()
+    return render_template('product/product_list.html', products=products)
+
+@bp.route('/detail/<int:product_id>/')
+def product_detail(product_id):
+    product_service = ProductService()
+    product = product_service.get_product_by_id(product_id)
+
+    if product:
+        return render_template('product/product_detail.html', product=product)
+    else:
+        return render_template('product/product_not_found.html')
+
+
+@bp.route('/discontinue/<int:product_id>/', methods=['GET', 'POST'])
+def discontinue_product(product_id):
+    if request.method == 'POST':
+        try:
+            product_service = ProductService()
+            result = product_service.discontinue_product(product_id)
+
+            if result:
+                return redirect(url_for('product.product_list'))
+            else:
+                return render_template('product/product_not_found.html')
+
+        except ValueError:
+            return render_template('invalid_product_id.html')
+
+    return redirect(url_for('product.product_list'))
--- app/services/product_services.py
+++ app/services/product_services.py
@@ -3,3 +3,18 @@ from app.models.product import Product
 class ProductService:
     def get_product_list(self):
         return Product.select()
+
+    def get_product_by_id(self, product_id):
+        try:
+            return Product.get(Product.product_id == product_id)
+        except Product.DoesNotExist:
+            return None
+
+    def discontinue_product(self, product_id):
+        try:
+            product = Product.get(Product.product_id == product_id)
+            product.product_discontinued = True
+            product.save()
+            return True
+        except Product.DoesNotExist:
+            return False
