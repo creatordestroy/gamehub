777e6a9b13d43aebf2c88d1f2ac99032ec89ac63
robertjcalistri <robertjcalistri@gmail.com>
2023-07-24 09:40:22 -0400

    Reworked all reviews methods to take input from html form, and to have consistent style

--- app/routes/reviews.py
+++ app/routes/reviews.py
@@ -1,42 +1,47 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from app.services.review_services import ReviewService
 from flask import render_template
 
 bp = Blueprint('reviews', __name__)
 
+#Default reviews route, returns list of all product reviews
 @bp.route('/list/', methods=['GET'])
 def review_list():
     reviews = ReviewService.review_list()
     return render_template('/reviews/review_list.html', reviews=reviews)
 
-@bp.route('/all/', methods=['GET'])
-def get_all_product_reviews():
-    json_list, status_code = ReviewService.get_all_product_reviews()
+#Returns list of product reviews by product_id
+@bp.route('/id/', methods=['GET','POST'])
+def get_product_reviews_by_id():
 
-    return render_template('base.html', json_list = json_list), status_code
+    reviews = []
 
-@bp.route('/id/<int:product_id>/', methods=['GET'])
-def get_product_reviews_by_id(product_id):
-    json_list, status_code = ReviewService.get_product_reviews_by_id(product_id)
-    #return jsonify(reviews_data), status_code
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+        reviews = ReviewService.get_product_reviews_by_id(data['product_id'])
+    
+    return render_template('/reviews/review_by_id.html', reviews=reviews)
 
-    return render_template('base.html', json_list = json_list), status_code
-
-@bp.route('/product/', methods=['GET'])
+#Returns list of product reviews by product name
+@bp.route('/name/', methods=['GET', 'POST'])
 def get_product_reviews_by_name():
-    product_name = request.args.get('name')
-    print(product_name)
-    if product_name:
-        json_list, status_code = ReviewService.get_product_reviews_by_product_name(product_name)
-    #return jsonify(reviews_data), status_code
-    return render_template('base.html', json_list = json_list), status_code
 
-@bp.route('/user/', methods=['GET'])
-def get_product_reviews_by_user_id():
-    product_user = request.args.get('user')
-    print(product_user)
-    if product_user:
-        json_list, status_code = ReviewService.get_product_reviews_by_user_id(product_user)
-    #return jsonify(reviews_data), status_code
-    return render_template('base.html', json_list = json_list), status_code
+    reviews = []
+
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+        reviews = ReviewService.get_product_reviews_by_name(data['product_name'])
+    
+    return render_template('/reviews/review_by_name.html', reviews=reviews)
+
+#Returns list of product reviews by user name
+@bp.route('/user/', methods=['GET', 'POST'])
+def get_product_reviews_by_user():
+    reviews = []
+
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+        reviews = ReviewService.get_product_reviews_by_user(data['user_id'])
+    
+    return render_template('/reviews/review_by_user.html', reviews=reviews)
 
--- app/services/review_services.py
+++ app/services/review_services.py
@@ -1,30 +1,13 @@
 from app.models.product import Product
-from app.models.user import User
 from app.models.reviews import Reviews
-from flask import jsonify
-from app.database import db
-from peewee import JOIN
 
 class ReviewService:
+
+    #Function for selecting all reviews
     def review_list():
         return Reviews.select()
 
-    def get_all_product_reviews():
-        try:
-            product_reviews = Reviews.select()
-
-            if product_reviews:
-
-                reviews_data = [review.__data__ for review in product_reviews]
-
-                #return {'message': 'Successfully retrieved test product', 'product_data': reviews_data}, 200
-                return reviews_data, 200
-            else:
-                return {'message': 'No products found in the inventory'}, 404
-
-        except Exception as e:
-            return {'error': str(e)}, 500
-
+    #Function for selecting all reviews by product id
     def get_product_reviews_by_id(product_id):
 
         try:
@@ -32,20 +15,15 @@ class ReviewService:
 
             if product_reviews:
 
-                reviews_data = [{
-                    'rating' : review.product_rating,
-                    'comment' : review.product_review
-                } for review in product_reviews]
-                #reviews_data = [review.product_review for review in product_reviews]
-
-                return reviews_data, 200
+                return product_reviews
             else:
                 return {'message': 'No product reviews found in the inventory'}, 404
 
         except Exception as e:
             return {'error': str(e)}, 500
 
-    def get_product_reviews_by_product_name(product_name):
+    #Function for selecting all reviews by product name
+    def get_product_reviews_by_name(product_name):
 
         try:
             product_id = Product.get(Product.product_name == product_name).product_id
@@ -53,35 +31,22 @@ class ReviewService:
 
             if product_reviews:
 
-                reviews_data = [{
-                    'name' : product_name,
-                    'rating' : review.product_rating,
-                    'review' : review.product_review
-                } for review in product_reviews]
-
-                return reviews_data, 200
+                return product_reviews
             else:
                 return {'message' : 'No product reviews found for product name'}, 404
 
         except Exception as e:
             return {'error': str(e)}, 500
 
-    def get_product_reviews_by_user_id(user_id):
+    #Function for selecting all reviews by user name
+    def get_product_reviews_by_user(user_id):
 
         try:
-            user_name = User.get(User.user_id == user_id).user_name
             user_reviews = Reviews.select().where(Reviews.user_id == user_id)
 
             if user_reviews:
 
-                reviews_data = [{
-                    'username' : user_name,
-                    'rating' : review.product_rating,
-                    'review' : review.product_review,
-                    'date' : review.review_date
-                }for review in user_reviews]
-
-                return reviews_data, 200
+                return user_reviews
             else:
                 return {'message' : 'No product reviews found for product name'}, 404
 
