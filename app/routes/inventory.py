82d4f911ed93bd4ff121baa3339207ea8571b3dd
robertjcalistri <robertjcalistri@gmail.com>
2023-07-24 14:06:33 -0400

    Updated half of inventory search functions to take input through html form

--- app/routes/inventory.py
+++ app/routes/inventory.py
@@ -3,13 +3,9 @@ from flask import Blueprint, request, jsonify, render_template
 from app.services.inventory_services import InventoryService
 from app.services.store_location_services import StoreLocationService
 from app.services.product_services import ProductService
+from flask import redirect, url_for
 
 bp = Blueprint('inventory', __name__)
-
-#Test method for inventory route
-@bp.route('/test/', methods=['GET'])
-def test():
-    return jsonify({'message': 'Inventory test route'}), 200
 
 #Route for display all current inventory objects in all stores
 @bp.route('/list/', methods=['GET'])
@@ -23,7 +19,6 @@ def add_inventory():
 
     if request.method == 'POST':
         data = request.form.to_dict(flat=True)
-        print(data)
         inventory = InventoryService().add_inventory(data['product_id'], data['store_id'], data['product_cost'], data['product_quantity'])
         return "success", 201
     
@@ -31,40 +26,38 @@ def add_inventory():
     all_products = ProductService().get_product_list()
     return render_template('inventory/add_inventory.html', store_locations = all_stores, products = all_products)
 
-@bp.route('/<int:inventory_id>/', methods=['PUT'])
-def update_inventory(inventory_id):
-    data = request.form.to_dict(flat=True)
-    inventory = InventoryService().update_inventory(inventory_id, data['count'])
-    return jsonify(inventory), 200
 
-@bp.route('/search/', methods=['GET'])
-def search_product_by_name():
+#Route for searching for a product by name in any store
+@bp.route('/search/name/', methods=['GET', 'POST'])
+def search_by_product_name():
 
-    product_name = request.args.get('name')
-    if product_name:
-        json_list, status_code = InventoryService.product_search_by_name(product_name)
-        print(json_list)
+    product_list = []
 
-    return render_template('inventory/table_template.html', json_list=json_list), status_code
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+        product_name = data['product_name']
 
-@bp.route('/search/<string:product_id>/', methods=['GET'])
-def search_product_by_id(product_id):
+        return redirect(url_for('inventory.display_search_id', product_name = product_name))
 
-    if product_id:
-        json_list, status_code = InventoryService.product_search_by_id(product_id)
-        print(json_list)
+    return render_template('inventory/inv_search_name.html', product_list=product_list)
 
-    return render_template('inventory/table_template.html', json_list=json_list), status_code
 
-@bp.route('/search/store/<string:store_id>/', methods=['GET'])
-def search_product_by_store_id(store_id):
+#Route for searching for a product by id in any store
+@bp.route('/search/id/', methods=['GET','POST'])
+def search_by_product_id():
 
-    if store_id:
-        json_list, status_code = InventoryService.product_search_by_store_id(store_id)
-        print(json_list)
+    product_list = []
 
-    return render_template('inventory/table_template.html', json_list=json_list), status_code
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+        product_id = data['product_id']
 
+        return redirect(url_for('inventory.display_search_id', product_id = product_id))
+
+    return render_template('inventory/inv_search_id.html', product_list=product_list)
+
+
+#Route for searching for a product in a specific store
 @bp.route('/search/storeid/<int:product_id>+<int:store_id>/', methods=['GET'])
 def search_product_by_id_in_store(product_id, store_id):
 
@@ -74,3 +67,15 @@ def search_product_by_id_in_store(product_id, store_id):
         print(json_list)
     #return jsonify(json_list), status_code
     return render_template('inventory/table_template.html', json_list=json_list), status_code
+
+@bp.route('/search/results', methods=['GET'])
+def display_search_id():
+    # Retrieve the product_list from the request's query parameters
+    
+    product_name = request.args.get('product_name')
+
+    product_list = InventoryService.search_by_product_name(product_name)
+    product_cost = product_list['cost']
+    product_list = product_list['details']
+    
+    return render_template('inventory/inv_search_results.html', product_list=product_list, product_cost=product_cost)
